f2a285dfbd7585374362ddd5b72094d79ebd87ac
Nivetha-deve <nivethashree028@gmail.com>
2024-06-14 20:10:21 +0530

    added

--- router/db-utils/mongo-connection.js
+++ router/db-utils/mongo-connection.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 const dbCluster = process.env.DB_CLUSTER || "localhost:27017";
 const dbName = process.env.DB_NAME || "tamil-weekend";
-const dbUserName = process.env.DB_USER || "";
-const dbPassword = process.env.DB_PASSWORD || "";
+const dbUserName = encodeURIComponent(process.env.DB_USER || "");
+const dbPassword = encodeURIComponent(process.env.DB_PASSWORD || "");
 
 const cloudUri = `mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Nivetha`;
 
